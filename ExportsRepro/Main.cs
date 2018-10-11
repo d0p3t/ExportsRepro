e2d2ecbfca6f736981d05bb3249ae5ceed8eeb4b
d0p3t <remcotroost@outlook.com>
2018-10-11 17:51:21 +0200

    Push

---
+++ ExportsRepro/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace ExportsRepro
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+            EventHandlers.Add("onClientMapStart", new Action(OnClientMapStart));
+        }
+
+        private void OnClientMapStart()
+        {
+            Exports["spawnmanager"].setAutoSpawn(true);
+            Exports["spawnmanager"].forceRespawn();
+        }
+    }
+}
